5da54c906641bbce342beed5ba649cb545a3efb5
Kevin Zhao <kevinzha@microsoft.com>
2016-09-22 21:25:05 +0800

    refine gulp tasks for lesson 3

--- raspberrypi-node-az-blink.js
+++ raspberrypi-node-az-blink.js
@@ -1,9 +1,11 @@
 var raspberrypi = require('./raspberrypi-node.js');
 
-// For DEMO purpose. We can discuss later whether we should put it to gulp-common.
 var all = require('./all.js');
+var EventHubClient = require('azure-event-hubs').Client;
 var exec = require('child_process').exec;
 var moment = require('moment');
+var Q = require('q');
+var simssh = require('simple-ssh');
 var storage = require('azure-storage');
 
 var config = (all.fileExistsSync('../config.json')) ? require('../config.json') : require('../../config.json');
@@ -12,8 +14,61 @@ var params = (all.fileExistsSync('../arm-template-param.json')) ? require('../ar
 function initTasks(gulp) {
   raspberrypi.initTasks(gulp);
 
-  // TODO: allow user to pass resource group as parameter if default value is not what they want.
-  gulp.task('read-message', function () {
+  gulp.task('run', 'Runs deployed sample on the board', function () {
+    var deferred = Q.defer();
+
+    // Listen device-to-cloud messages
+    var printError = function (err) {
+      console.log(err.message);
+    };
+    var printMessage = function (message) {
+      console.log('Message received: \n' + JSON.stringify(message.body) + '\n');
+    };
+    var iotHubClient = EventHubClient.fromConnectionString(config.iot_hub_connection_string);
+    iotHubClient.open()
+      .then(iotHubClient.getPartitionIds.bind(iotHubClient))
+      .then(function (partitionIds) {
+        return partitionIds.map(function (partitionId) {
+          return iotHubClient.createReceiver('$Default', partitionId, { 'startAfterTime': Date.now() - 10000 }).then(function (receiver) {
+            receiver.on('errorReceived', printError);
+            receiver.on('message', printMessage);
+          });
+        });
+      })
+      .catch(printError);
+
+    // Send command to device
+    var ssh = new simssh({
+      host: config.device_host_name_or_ip_address,
+      user: config.device_user_name,
+      pass: config.device_password
+    });
+
+    ssh.on('error', function (e) {
+      // when we pass error via deferred.reject, stack will be displayed
+      // as it is just string, we can just replace it with message
+      e.stack = "ERROR: " + e.message;
+      deferred.reject(e);
+    });
+
+    var targetFolder = config.project_folder ? config.project_folder : '.';
+    var startFile = config.start_file ? config.start_file : 'az-blink.js';
+    ssh.exec('sudo nodejs ' + targetFolder + '/' + startFile, {
+      pty: true,
+      out: function (o) { console.log(o); }, // Always log to console when running.
+      exit: function() { 
+        iotHubClient.close();
+        deferred.resolve();
+      }
+    }).start();
+
+    return deferred.promise;
+  });
+
+
+  gulp.task('run-with-az-func', function () {
+    // TODO: az command has changed to 'az storage account show-connection-string' in latest packages.
+    // We need to update command here when using latest AZ bits.
     var command = 'az storage account connection-string -g ' + config.resource_group + ' -n ' + params.resoucePrefix.value + 'storage';
     exec(command, function (err, stdout, stderr) {
       if (err) {
@@ -30,6 +85,7 @@ function initTasks(gulp) {
           var condition = 'PartitionKey eq ? and RowKey gt ? ';
           var tableName = 'DeviceData';
           var timestamp = moment.utc().format('hhmmssSSS');
+          var isCancelled = false;
 
           function readNewMessage() {
             var query = new storage.TableQuery().where(condition, moment.utc().format('YYYYMMDD'), timestamp);
@@ -41,26 +97,50 @@ function initTasks(gulp) {
                 return;
               }
 
-              timestamp = moment.utc().format('hhmmssSSS');
-
               // result.entries contains entities matching the query
-              if (result.entries.length == 0) {
-                console.log('\nNo New Message.');
-              } else {
+              if (result.entries.length > 0) {
                 console.log('\nNew Messages:');
                 for (var i = 0; i < result.entries.length; i++) {
                   console.log(result.entries[i].message['_']);
-                  // Update timestamp for next table query
                   if (result.entries[i].RowKey['_'] > timestamp) {
                     timestamp = result.entries[i].RowKey['_'];
                   }
                 }
               }
-              setTimeout(readNewMessage, 0);
+              if (!isCancelled) {
+                setTimeout(readNewMessage, 0);
+              }
             });
           }
-
           readNewMessage();
+
+          // Send command to device
+          var deferred = Q.defer();
+          var ssh = new simssh({
+            host: config.device_host_name_or_ip_address,
+            user: config.device_user_name,
+            pass: config.device_password
+          });
+
+          ssh.on('error', function (e) {
+            // when we pass error via deferred.reject, stack will be displayed
+            // as it is just string, we can just replace it with message
+            e.stack = "ERROR: " + e.message;
+            deferred.reject(e);
+          });
+
+          var targetFolder = config.project_folder ? config.project_folder : '.';
+          var startFile = config.start_file ? config.start_file : 'az-blink.js';
+          ssh.exec('sudo nodejs ' + targetFolder + '/' + startFile, {
+            pty: true,
+            out: function (o) { console.log(o); }, // Always log to console when running.
+            exit: function () {
+              isCancelled = true;
+              deferred.resolve();
+            }
+          }).start();
+
+          return deferred.promise;
         } else {
           console.error('ERROR: Fail to get connection string of Azure Storage account.')
         }
