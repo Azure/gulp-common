ae6fdc33d93ea5efa9b7cc8fe882d01b808314e7
FAREAST\zhijzhao <zhijzhao@microsoft.com>
2016-09-23 15:45:55 +0800

    rework gulpfile for raspberry pi node

--- raspberrypi-node-blink.js
+++ raspberrypi-node-blink.js
@@ -2,10 +2,10 @@ var all = require('./all.js');
 var config = (all.fileExistsSync('../config.json')) ? require('../config.json') : require('../../config.json');
 var simssh = require('simple-ssh');
 var Q = require('q');
-var args = require('get-gulp-args')();
+var  args  =  require('get-gulp-args')();
 
 function initTasks(gulp) {
-  var runSequence = require('run-sequence').use(gulp);
+  var  runSequence  =  require('run-sequence').use(gulp);
 
   if (typeof all.gulpTaskBI === 'function') {
     all.gulpTaskBI(gulp, 'nodejs', 'RaspberryPi', 'blink');
@@ -15,13 +15,13 @@ function initTasks(gulp) {
     all.azhSshExec('sudo apt-get -y update && sudo apt-get -y install npm', config, args.verbose, cb);
   });
 
-  gulp.task('deploy', 'Deploys sample code to the board', function(){
+  gulp.task('deploy', 'Deploys sample code to the board', function () {
     var deferred = Q.defer();
-    
+
     var targetFolder = config.project_folder ? config.project_folder : '.';
-    all.uploadFilesViaScp(config, ["./blink.js", "./device-package.json"], [targetFolder + "/blink.js", targetFolder + "/package.json"], function(){
+    all.uploadFilesViaScp(config, ["./blink.js", "./device-package.json"], [targetFolder + "/blink.js", targetFolder + "/package.json"], function () {
       console.log("- Installing npm packages on device");
-      
+
       var ssh = new simssh({
         host: config.device_host_name_or_ip_address,
         user: config.device_user_name,
@@ -38,10 +38,10 @@ function initTasks(gulp) {
       ssh.exec('cd ' + targetFolder + ' && npm install', {
         pty: true,
         out: function (o) { if (args.verbose) console.log(o); },
-        exit: function() { deferred.resolve(); }
+        exit: function () { deferred.resolve(); }
       }).start();
     });
-    
+
     return deferred.promise;
   });
 
@@ -66,13 +66,13 @@ function initTasks(gulp) {
     ssh.exec('sudo nodejs ' + targetFolder + '/' + startFile + ' && exit', {
       pty: true,
       out: console.log.bind(console),
-      exit: function() { deferred.resolve(); }
+      exit: function () { deferred.resolve(); }
     }).start();
 
     return deferred.promise;
   });
 
-  gulp.task('default', 'Builds, deploys and runs sample on the board', function(callback) {
+  gulp.task('default', 'Builds, deploys and runs sample on the board', function (callback) {
     runSequence('install-tools', 'deploy', 'run', callback);
   })
 }
