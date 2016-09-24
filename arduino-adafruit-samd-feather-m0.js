58390a6fb60bd9e4e64bb799d537d0edbafd27d2
Zim Kalinowski <zikalino@microsoft.com>
2016-09-24 17:16:20 +0800

    fixed options structure

--- arduino-adafruit-samd-feather-m0.js
+++ arduino-adafruit-samd-feather-m0.js
@@ -1,7 +1,7 @@
 ﻿'use strict';
 
 function initTasks(gulp) {
-  require('./arduino-adafruit.js')(gulp, { board: { board: 'adafruit_feather_m0', parameters: '', libraries: [] }});
+  require('./arduino-adafruit.js')(gulp, { board: { board: 'adafruit_feather_m0', parameters: '' }, libraries: []});
 }
 
 module.exports = initTasks;
