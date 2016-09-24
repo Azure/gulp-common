afee8a9b5fe361e7ad4c39f0bad8669a5b78b27e
Zim Kalinowski <zikalino@microsoft.com>
2016-09-24 20:00:58 +0800

    added comments in index

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 ﻿'use strict';
 
+// [REVIEW] wrong boardId supplied shall be handled gracefully
+
+/**
+ * Main entry point for gulp-common.
+ * @param {object} gulp     - external gulp instance
+ * @param {string} boardId  - Board identifier. Must match one of the predefined boards.
+ * @param {object} options  - options depending on particular board requirements
+ */
 function initTasks(gulp, boardId, options) {
-  return require('./' + boardId + '.js')(require('gulp-help')(gulp), options);
+  require('./' + boardId + '.js')(require('gulp-help')(gulp), options);
 }
 
 module.exports = initTasks;
