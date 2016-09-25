1c638572929a9fb0c708d238c0497fcb0fcaccde
Zim Kalinowski <zikalino@microsoft.com>
2016-09-25 17:46:38 +0800

    arduino options can be passed externally now

--- arduino-esp8266-thingdev.js
+++ arduino-esp8266-thingdev.js
@@ -4,8 +4,17 @@
  * Main entry point for all Arduino ESP8266 ThingDev configuration.
  * @param {object} gulp     - Gulp instance
  */
-function initTasks(gulp) {
-  require('./arduino-esp8266.js')(gulp, { board: { board: 'thingdev', parameters: ''}, libraries: [] });
+function initTasks(gulp, options) {
+
+  // initialize options if undefined
+  if (typeof options != 'object') options = {};
+  if (typeof options.libraries == 'undefined') options.libraries = [];
+  if (typeof options.board == 'undefined') options.board = {};
+
+  options.board.board = 'thingdev';
+  options.board.parameters = '';
+
+  require('./arduino-esp8266.js')(gulp, options);
 }
 
 module.exports = initTasks;
