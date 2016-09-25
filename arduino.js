dc393c0542b2e155f8d9e023cc1f8b083ed9f63c
Zim Kalinowski <zikalino@microsoft.com>
2016-09-26 07:01:15 +0800

    more fixes related to downloading & unzipping arduino on linux

--- arduino.js
+++ arduino.js
@@ -57,10 +57,10 @@ function initTasks(gulp, options) {
       // install arduino
       all.localExecCmds([ 'sudo apt-get update',
                           'sudo apt-get install -y wget xz-utils',
-                          'sudo wget -q -O '  + all.getToolsFolder() + '/arduino.tar.xz' + ' https://downloads.arduino.cc/arduino-1.6.11-linux64.tar.xz',
-                          'sudo tar xJ -C /opt ' + all.getToolsFolder() + '/arduino.tar.xz' ,
-                          'ln -s /opt/arduino-1.6.11/arduino /usr/local/bin/',
-                          'ln -s /opt/arduino-1.6.11/arduino-builder /usr/local/bin/',
+                          'sudo wget --output-document='  + all.getToolsFolder() + '/arduino.tar.xz' + ' https://downloads.arduino.cc/arduino-1.6.11-linux64.tar.xz',
+                          'sudo tar xJ --file=' + all.getToolsFolder() + '/arduino/tar.xz -C ' + all.getToolsFolder() ,
+                          'ln -s ' + all.getToolsFolder() + '/arduino-1.6.11/arduino /usr/local/bin/',
+                          'ln -s ' + all.getToolsFolder() + '/arduino-1.6.11/arduino-builder /usr/local/bin/',
                           'chmod 777 gulp-common/arduino-headless.sh'], args.verbose, cb);
     } else if (process.platform == 'darwin') {
       // at the moment we will attempt the same approach as for windows
