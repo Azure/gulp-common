cfcb2d7cf38172d93cdeeff925134438e2f94ffa
Zim Kalinowski <zikalino@microsoft.com>
2016-09-26 12:04:17 +0800

    first attempt to refactor rpi node stuff

--- raspberrypi-node.js
+++ raspberrypi-node.js
@@ -1,105 +1,53 @@
-﻿'use strict';
+'use strict';
 
 var all = require('./all.js');
 var config = (all.fileExistsSync('../config.json')) ? require('../config.json') : require('../../config.json');
-var simssh = require('simple-ssh');
-var Q = require('q');
-var args = require('get-gulp-args')();
+var args = require('get-gulp-args')();
+var fs = require('fs');
 
 function initTasks(gulp) {
-  var runSequence = require('run-sequence').use(gulp);
+  var runSequence = require('run-sequence').use(gulp);
 
   if (typeof all.gulpTaskBI === 'function') {
-    all.gulpTaskBI(gulp, 'nodejs', 'RaspberryPi', 'az-blink');
+    all.gulpTaskBI(gulp, 'nodejs', 'RaspberryPi', 'blink');
   }
 
-  gulp.task('install-tools', 'Installs required software on Raspberry Pi', function () {
-    var deferred = Q.defer();
-    var input = 'Y\r';
-    var ssh = new simssh({
-      host: config.device_host_name_or_ip_address,
-      user: config.device_user_name,
-      pass: config.device_password
-    });
-
-    ssh.on('error', function (e) {
-      // when we pass error via deferred.reject, stack will be displayed
-      // as it is just string, we can just replace it with message
-      e.stack = "ERROR: " + e.message;
-      deferred.reject(e);
-    });
-
-    ssh.exec('sudo apt-get update && sudo apt-get install npm', {
-      pty: true,
-      in: input,
-      out: function (o) { if (args.verbose) console.log(o); },
-      exit: function() { deferred.resolve(); }
-    }).start();
-    
-    return deferred.promise;
+  gulp.task('install-tools', 'Installs required software on Raspberry Pi', function (cb) {
+    all.azhSshExec('(curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -) && sudo apt-get -y install nodejs', config, args.verbose, cb);
   });
 
-  gulp.task('deploy', 'Deploys compiled sample to the board', function(){
-    var deferred = Q.defer();
-
+  gulp.task('deploy', 'Deploys sample code to the board', function (cb) {
     var targetFolder = config.project_folder ? config.project_folder : '.';
-    all.uploadFilesViaScp(config, ['az-blink.js', 'device-package.json', 'config.json'], [targetFolder + '/az-blink.js', targetFolder + '/package.json', targetFolder + '/config.json'], function(){
-      console.log("- Installing npm packages on device");
-      
-      var ssh = new simssh({
-        host: config.device_host_name_or_ip_address,
-        user: config.device_user_name,
-        pass: config.device_password
-      });
+    var files = fs.readdirSync('./app');
+    var filesLocal = [];
+    var filesRemote = [];
+    console.log(files);
 
-      ssh.on('error', function (e) {
-        // when we pass error via deferred.reject, stack will be displayed
-        // as it is just string, we can just replace it with message
-        e.stack = "ERROR: " + e.message;
-        deferred.reject(e);
-      });
+    for (var i = 0; i < files.length; i++) {
+      filesLocal.push('./app/' + files[i]);
+      filesRemote.push(targetFolder + '/' + files[i]);
+    }
 
-      ssh.exec('cd ' + targetFolder + ' && npm install', {
-        pty: true,
-        out: function (o) { if (args.verbose) console.log(o); },
-        exit: function() { deferred.resolve(); }
-      }).start();
+    all.uploadFilesViaScp(config, filesLocal, filesRemote, function () {
+      // [REVIEW] failure is not handled properly
+      all.azhSshExec('cd ' + targetFolder + ' && npm install', config, args.verbose, cb);
     });
-
-    return deferred.promise;
   });
-
 
   gulp.task('run', 'Runs deployed sample on the board', function () {
-    var deferred = Q.defer();
-
-    var ssh = new simssh({
-      host: config.device_host_name_or_ip_address,
-      user: config.device_user_name,
-      pass: config.device_password
-    });
-
-    ssh.on('error', function (e) {
-      // when we pass error via deferred.reject, stack will be displayed
-      // as it is just string, we can just replace it with message
-      e.stack = "ERROR: " + e.message;
-      deferred.reject(e);
-    });
-
     var targetFolder = config.project_folder ? config.project_folder : '.';
-    var startFile = config.start_file ? config.start_file : 'az-blink.js';
-    ssh.exec('sudo nodejs ' + targetFolder + '/' + startFile, {
-      pty: true,
-      out: function (o) { console.log(o); }, // Always log to console when running.
-      exit: function() { deferred.resolve(); }
-    }).start();
+    var startFile = config.start_file ? config.start_file : 'blink.js';
+    var nodeCommand = 'nodejs';
+    if (args.debug) {
+      nodeCommand += ' --debug-brk=5858';
+    }
 
-    return deferred.promise;
+    all.azhSshExec('sudo' + ' ' + nodeCommand + ' ' + targetFolder + '/' + startFile + ' && exit', cb);
   });
 
-  gulp.task('default', 'Builds, deploys and runs sample on the board', function(callback) {
+  gulp.task('default', 'Builds, deploys and runs sample on the board', function (callback) {
     runSequence('install-tools', 'deploy', 'run', callback);
   })
 }
 
-module.exports.initTasks = initTasks;
+module.exports = initTasks;
