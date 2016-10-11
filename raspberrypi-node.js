d30204d7ffcb6130be245a05bf0881b6fdb46452
Zhijun Zhao <zhijzhao@microsoft.com>
2016-10-11 10:54:48 +0800

    Improve the device Nodejs version detecting and updating process

--- raspberrypi-node.js
+++ raspberrypi-node.js
@@ -21,7 +21,10 @@ function initTasks(gulp, options) {
   }
 
   gulp.task('install-tools', 'Installs required software on Raspberry Pi', function (cb) {
-    all.sshExecCmd("(node -v | grep -v 'v4.') && (curl -sL http://deb.nodesource.com/setup_4.x --output ~/setup_4.x --write-out %{http_code} | grep 200) && sudo -E bash ~/setup_4.x && sudo apt-get -y install nodejs", config, { verbose: args.verbose }, cb);
+    var ifNodeExist = "hash node 2>/dev/null";
+    var ifNode4xOr6xExists = "((node -v | grep -q 'v4.') || (node -v | grep -q 'v6.'))";
+    var installNode4x = "((curl -sL http://deb.nodesource.com/setup_4.x --output ~/setup_4.x --write-out %{http_code} | grep 200) && sudo -E bash ~/setup_4.x && sudo apt-get -y install nodejs)";
+    all.sshExecCmd("(" + ifNodeExist + "&&" + ifNode4xOr6xExists + ") || " + installNode4x, config, { verbose: args.verbose }, cb);
   });
 
   gulp.task('deploy', 'Deploys sample code to the board', function (cb) {
