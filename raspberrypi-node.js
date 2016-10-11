273451f30e44fdcd0035f0849875ef6866b4403f
ZhijunZhao <zhijun.zhao@outlook.com>
2016-10-11 11:30:02 +0800

    Improve the device Nodejs version detecting and updating process (#29)
    * Improve the device Nodejs version detecting and updating process
    
    * minor cleanup

--- raspberrypi-node.js
+++ raspberrypi-node.js
@@ -21,7 +21,10 @@ function initTasks(gulp, options) {
   }
 
   gulp.task('install-tools', 'Installs required software on Raspberry Pi', function (cb) {
-    all.sshExecCmd("(node -v | grep -v 'v4.') && (curl -sL http://deb.nodesource.com/setup_4.x --output ~/setup_4.x --write-out %{http_code} | grep 200) && sudo -E bash ~/setup_4.x && sudo apt-get -y install nodejs", config, { verbose: args.verbose }, cb);
+    var ifNodeExist = "hash node 2>/dev/null";
+    var ifNode4xOr6xExists = "(node -v | grep -q 'v[4|6]\.')";
+    var installNode4x = "((curl -sL http://deb.nodesource.com/setup_4.x --output ~/setup_4.x --write-out %{http_code} | grep 200) && sudo -E bash ~/setup_4.x && sudo apt-get -y install nodejs)";
+    all.sshExecCmd("(" + ifNodeExist + "&&" + ifNode4xOr6xExists + ") || " + installNode4x, config, { verbose: args.verbose }, cb);
   });
 
   gulp.task('deploy', 'Deploys sample code to the board', function (cb) {
@@ -54,7 +57,7 @@ function initTasks(gulp, options) {
     all.sshExecCmd('sudo' + ' ' + nodeCommand + ' ' + targetFolder + '/' + startFile + ' && exit', config, { verbose: true }, cb);
   });
 
-  gulp.task('run', 'Runs deployed sample on the board', [ 'run-internal' ]);
+  gulp.task('run', 'Runs deployed sample on the board', ['run-internal']);
 
   gulp.task('default', 'Builds, deploys and runs sample on the board', function (callback) {
     runSequence('install-tools', 'deploy', 'run', callback);
