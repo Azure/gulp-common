623b3186ed7b9a7b0a546b5c325c2601cf69d961
Zim Kalinowski <zikalino@microsoft.com>
2016-10-18 15:00:59 +0800

    added path to azure shared utility

--- raspberrypi-c-on-device.js
+++ raspberrypi-c-on-device.js
@@ -70,7 +70,7 @@ function initTasks(gulp, options) {
     var cmdCompile = 'arm-linux-gnueabihf-gcc ' +
       //'-I' + PREBUILT_FOLDER + '/inc/wiringpi ' +
       //'-I' + PREBUILT_FOLDER + '/inc/serializer ' +
-      //'-I' + PREBUILT_FOLDER + '/inc/azure-c-shared-utility ' +
+      '-I~/azure-iot-sdks/c/azure-c-shared-utility/inc/azure_c_shared_utility ' +
       //'-I' + PREBUILT_FOLDER + '/inc/platform_specific ' +
       //'-I' + PREBUILT_FOLDER + '/inc ' +
       //'-I' + PREBUILT_FOLDER + '/inc/iothub_client ' +
