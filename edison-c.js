d79336491edf72bf3aa6268ddc687d98fa8da367
Zhijun Zhao <zhijzhao@microsoft.com>
2016-11-02 19:59:29 +0800

    add gulp tasks for building Azure IoT SDK

--- edison-c.js
+++ edison-c.js
@@ -3,6 +3,7 @@
 */
 'use strict';
 
+var fs = require('fs');
 var args = require('get-gulp-args')();
 
 var all;
@@ -30,11 +31,67 @@ function initTasks(gulp, options) {
     }
 
     cb();
-  })
+  });
 
-  gulp.task('install-tools', 'Installs required software on the device', function (cb) {
+  gulp.task('install-mraa', false, function (cb) {
     all.sshExecCmd("opkg install mraa", { verbose: args.verbose }, cb);
   });
+
+  gulp.task('clone-iot-sdk', false, function (cb) {
+    var repoFolderPath = all.getToolsFolder() + '/' + 'azure-iot-sdks';
+    fs.exists(repoFolderPath, function (exists) {
+      if (exists) {
+        console.log('Azure IoT SDK repo already exists in folder ' + repoFolderPath);
+      } else {
+        console.log('Clone Azure IoT SDK to folder ' + repoFolderPath + '. It will take several minutes.');
+        all.sshExecCmds(["git clone --recursive https://github.com/Azure/azure-iot-sdks.git"],
+          {
+            verbose: args.verbose,
+            sshPrintCommands: true,
+            validate: true
+          }, cb);
+      }
+    });
+  });
+
+  gulp.task('upload-sdk-to-device', false, function (cb) {
+    // TODO: check the folder existence on device before copy.
+    // Console output: 1. already exists; 2. may take several minutes. 
+    var folderName = 'azure-iot-sdks';
+    var repoFolderPath = all.getToolsFolder() + '/' + folderName + '/';
+
+    var src = [];
+    src.push(repoFolderPath);
+    var target = [];
+    target.push('./' + folderName + '/');
+
+    all.uploadFilesViaScp(src, target, function (err) {
+      if (err) {
+        console.log(err);
+        if (cb) {
+          cb(err);
+        }
+      } else {
+        if (cb) {
+          cb();
+        }
+      }
+    });
+  });
+
+  gulp.task('build-iot-sdk-on-device', false, function (cb) {
+    all.sshExecCmds(["cd ~/azure-iot-sdks && sudo c/build_all/linux/build.sh --skip-unittests"],
+      {
+        verbose: args.verbose,
+        sshPrintCommands: true,
+        validate: true
+      }, cb);
+  });
+
+  gulp.task('install-tools', 'Installs required software on the device', function (cb) {
+    runSequence('install-mraa', 'clone-iot-sdk', 'upload-sdk-to-device', 'build-iot-sdk-on-device', cb);
+  });
+
 
   gulp.task('deploy', 'Deploy and build sample code on the device', function (cb) {
     // write config file only if any
@@ -56,7 +113,7 @@ function initTasks(gulp, options) {
         cb(err);
       } else {
         all.sshExecCmds(['cd ' + targetFolder + ' && cmake .',
-          'cd ' + targetFolder + ' && make'],
+        'cd ' + targetFolder + ' && make'],
           {
             verbose: args.verbose,
             sshPrintCommands: true,
@@ -67,15 +124,20 @@ function initTasks(gulp, options) {
   });
 
   gulp.task('run-internal', false, function (cb) {
+    var param = '';
+    if (config.iot_device_connection_string) {
+      param = '"' + config.iot_device_connection_string + '"';
+    }
+
     all.sshExecCmd('sudo chmod +x ' + targetFolder + '/' + startFile + ' ; sudo '
-      + targetFolder + '/' + startFile, { verbose: true, sshPrintCommands: true }, cb);
+      + targetFolder + '/' + startFile + ' ' + param, { verbose: true, sshPrintCommands: true }, cb);
   });
 
   gulp.task('run', 'Runs deployed sample on the board', ['run-internal']);
 
   gulp.task('default', 'Deploys and runs sample on the board', function (callback) {
     runSequence('install-tools', 'deploy', 'run', callback);
-  })
+  });
 }
 
 module.exports = initTasks;
