c113b7274c9cdeca90ceae194c2d0653b34d4e20
Yaohai Zheng <yaozheng@microsoft.com>
2016-11-02 15:06:48 +0800

    Fix empty password issue. (#65)

--- all.js
+++ all.js
@@ -522,7 +522,7 @@ function updateGlobalConfig(postfix, template) {
 function writeConfigH() {
   if (config.iot_device_connection_string) {
     var headerContent = `static const char* connectionString = "${config.iot_device_connection_string}";`;
-    if (config.wifi_ssid && config.wifi_password) {
+    if (config.hasOwnProperty('wifi_ssid') && config.hasOwnProperty('wifi_password')) {
       headerContent =
         `${headerContent}${os.EOL}static const char* ssid="${config.wifi_ssid}";${os.EOL}static const char* pass="${config.wifi_password}";`;
     }
