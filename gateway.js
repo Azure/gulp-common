eecf811180ca13f9cddae93d0756e7e749aa7a1b
Yuwei Zhou <yuwzho@microsoft.com>
2016-11-04 14:58:20 +0800

    rename of gulp task for gateway

--- gateway.js
+++ gateway.js
@@ -11,30 +11,30 @@ var all;
 function initTasks(gulp, options) {
   all = require('./all.js')(options);
 
-  var config = all.getConfig();
+  var config = flatten(all.getConfig());
   var workspace = './ble_sample/';
 
   // stick config into gulp object
-  gulp.config = flatten(config);
+  gulp.config = config;
 
   if (typeof all.gulpTaskBI === 'function') {
     all.gulpTaskBI(gulp, 'nodejs', 'gateway', ((options && options.appName) ? options.appName : 'unknown'));
   }
 
   // copy files into profile folder
-  gulp.task('init', 'Initializes sample', function(cb) {
+  gulp.task('init', 'Initialize config files in the user’s profile folder', function(cb) {
 
     if (options.configPostfix && options.configTemplate) {
       all.updateGlobalConfig(options.configPostfix, options.configTemplate['ssh-config']);
-      saveConfigFile(config.bleConfig, options.configTemplate['ble-config']);
-      saveConfigFile(config.azFuncConfig, options.configTemplate['azure-function-config']);
+      saveConfigFile(config.sensortagConfig, options.configTemplate['sensortag-config']);
+      saveConfigFile(config.azureConfig, options.configTemplate['azure-config']);
     }
 
     cb();
   });
 
   // copy inital files to the NUC
-  gulp.task('setup-remote', 'Copy script to remote', function(cb) {
+  gulp.task('install-tools', 'Install necessary tools into the gateway', function(cb) {
     var cpList = [
       'app/.ble_gateway.json',
       'app/sensortagdisco.js',
@@ -74,7 +74,7 @@ function initTasks(gulp, options) {
   gulp.task('clean', 'clean local and remote', ['clean-remote', 'clean-local']);
 
   // discover sensortag device
-  gulp.task('devdisco', 'discovery Sensortag device', function(cb) {
+  gulp.task('discover-sensortag', 'Discover sensortag devices, need run "install-tools" first', function(cb) {
     all.sshExecCmd('cd ' + workspace + '; node sensortagdisco.js', {
       verbose: true
     }, function(err) {
@@ -88,7 +88,7 @@ function initTasks(gulp, options) {
 
   // test sensortag's connectivity
   // usage: gulp testconnect --mac <mac address>
-  gulp.task('testconnect', 'test connectivity of mac address', function(cb) {
+  gulp.task('test-connectivity', 'Test connectivity of the SensorTag, need run "install-tools" first', function(cb) {
     all.sshExecCmd('cd ' + workspace + '; node testconnect.js ' + args['mac'], {
       verbose: true
     }, function(err) {
@@ -101,7 +101,7 @@ function initTasks(gulp, options) {
   });
 
   // run BLE sample on NUC
-  gulp.task('run', 'run ble_sample on NUC', ['setup-remote', 'upload-config'], function(cb) {
+  gulp.task('run', 'Run the BLE sample application in the Gateway SDK', ['install-tools', 'upload-config'], function(cb) {
     all.sshExecCmd('cd ' + workspace + '; node run.js', {
       verbose: true
     }, function(err) {
@@ -113,29 +113,9 @@ function initTasks(gulp, options) {
     });
   });
 
-  // copy all needed files into NUC, and generate the ble_gateway.json
-  // usage: gulp deploy [options]
-  // options:
-  //     -g, --global: directly save the file into NUC's /usr/share/azureiotgatewaysdk/sample/ble_gateway/
-  //     -f, --force: not update the config base on global config, but always reset the ble_gateway.json to default
-  gulp.task('deploy', 'deplo ble_sample on NUC', ['setup-remote', 'upload-config'], function(cb) {
-    var force = args['force'] || args['f'];
-    var global = args['global'] || args['g'];
-    var options = '' + (force ? ' --force' : '') + (global ? ' --global' : '');
-    all.sshExecCmd('cd ' + workspace + '; node deploy.js' + options, {
-      verbose: true
-    }, function(err) {
-      if (err) {
-        cb(err);
-      } else {
-        cb();
-      }
-    });
-  });
-
   // copy config.json into NUC
-  gulp.task('upload-config', 'upload config.json to NUC', function(cb) {
-    all.uploadFilesViaScp([getConfigFilepath(config.bleConfig)], [workspace + 'config.json'], cb);
+  gulp.task('upload-config', 'Copy config file to the gateway machine', function(cb) {
+    all.uploadFilesViaScp([config.sensortagConfigPath], [workspace + 'config.json'], cb);
   });
 }
 
@@ -152,7 +132,7 @@ function getConfigFilepath(postfix) {
 
 function readConfig(filename) {
   if (all.fileExistsSync(filename)) {
-    return require(filename);
+    return JSON.parse(fs.readFileSync(filename, 'utf8'));
   }
 
   return {};
@@ -161,17 +141,16 @@ function readConfig(filename) {
 function flatten(rawConfig) {
   // path
   var config = {
-    bleConfigPath: getConfigFilepath(rawConfig.bleConfig),
-    azFuncConfigPath: getConfigFilepath(rawConfig.azFuncConfig)
+    sensortagConfigPath: getConfigFilepath(rawConfig.sensortagConfig),
+    azureConfigPath: getConfigFilepath(rawConfig.azureConfig)
   };
 
   // two object
-  var bleConfig = readConfig(config.bleConfigPath);
-  var azFuncConfig = readConfig(config.azFuncConfigPath);
+  var sensortagConfig = readConfig(config.sensortagConfigPath);
+  var azureConfig = readConfig(config.azureConfigPath);
 
   // merge
-  var mergeConfig = Object.assign(bleConfig, azFuncConfig);
-  return Object.assign(mergeConfig, rawConfig);
+  return Object.assign(config, sensortagConfig, azureConfig, rawConfig);
 }
 
 module.exports = initTasks;
